1ac3f12766b281ba9a4e3b80b2dab3f08de2fbd8
Mario <xmariosalman@gmail.com>
2023-03-20 19:11:57 +0200

    added terms page

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -1,24 +1,26 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 export function Terms() {
   return (
-    <div>
-      <strong>Terms and Conditions</strong>
+    <Box sx={{ m: 3 }}>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        Terms and Conditions
+      </Typography>
 
-      <p>Welcome to My-To-Do-Project!</p>
+      <Typography>Welcome to My-To-Do-Project!</Typography>
 
-      <p>
+      <Typography>
         These terms and conditions outline the rules and regulations for the use of Project Corporation 's Website,
         located at https://todoprojectfirebase.netlify.app.
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         By accessing this website we assume you accept these terms and conditions. Do not continue to use
         My-To-Do-Project if you do not agree to take all of the terms and conditions stated on this page.
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all
         Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the
         Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company.
@@ -28,47 +30,47 @@ export function Terms() {
         Company's stated services, in accordance with and subject to, prevailing law of ro. Any use of the above
         terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as
         interchangeable and therefore as referring to same.
-      </p>
+      </Typography>
 
-      <h3>
-        <strong>Cookies</strong>
-      </h3>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        Cookies
+      </Typography>
 
-      <p>
+      <Typography>
         We employ the use of cookies. By accessing My-To-Do-Project, you agreed to use cookies in agreement with the
         Project Corporation 's Privacy Policy.{" "}
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by
         our website to enable the functionality of certain areas to make it easier for people visiting our website. Some
         of our affiliate/advertising partners may also use cookies.
-      </p>
+      </Typography>
 
-      <h3>
-        <strong>License</strong>
-      </h3>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        License
+      </Typography>
 
-      <p>
+      <Typography>
         Unless otherwise stated, Project Corporation and/or its licensors own the intellectual property rights for all
         material on My-To-Do-Project. All intellectual property rights are reserved. You may access this from
         My-To-Do-Project for your own personal use subjected to restrictions set in these terms and conditions.
-      </p>
+      </Typography>
 
-      <p>You must not:</p>
-      <ul>
+      <Typography>You must not:</Typography>
+      <Typography sx={{ mx: 3 }}>
         <li>Republish material from My-To-Do-Project</li>
         <li>Sell, rent or sub-license material from My-To-Do-Project</li>
         <li>Reproduce, duplicate or copy material from My-To-Do-Project</li>
         <li>Redistribute content from My-To-Do-Project</li>
-      </ul>
+      </Typography>
 
-      <p>
+      <Typography>
         This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the{" "}
         <a href="https://www.termsfeed.com/terms-conditions-generator/">Terms and Conditions Generator</a>.
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         Parts of this website offer an opportunity for users to post and exchange opinions and information in certain
         areas of the website. Project Corporation does not filter, edit, publish or review Comments prior to their
         presence on the website. Comments do not reflect the views and opinions of Project Corporation ,its agents
@@ -76,16 +78,16 @@ export function Terms() {
         the extent permitted by applicable laws, Project Corporation shall not be liable for the Comments or for any
         liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or
         appearance of the Comments on this website.
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         Project Corporation reserves the right to monitor all Comments and to remove any Comments which can be
         considered inappropriate, offensive or causes breach of these Terms and Conditions.
-      </p>
+      </Typography>
 
-      <p>You warrant and represent that:</p>
+      <Typography>You warrant and represent that:</Typography>
 
-      <ul>
+      <Typography sx={{ m: 3 }}>
         <li>
           You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;
         </li>
@@ -101,20 +103,20 @@ export function Terms() {
           The Comments will not be used to solicit or promote business or custom or present commercial activities or
           unlawful activity.
         </li>
-      </ul>
+      </Typography>
 
-      <p>
+      <Typography>
         You hereby grant Project Corporation a non-exclusive license to use, reproduce, edit and authorize others to
         use, reproduce and edit any of your Comments in any and all forms, formats or media.
-      </p>
+      </Typography>
 
-      <h3>
-        <strong>Hyperlinking to our Content</strong>
-      </h3>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        Hyperlinking to our Content
+      </Typography>
 
-      <p>The following organizations may link to our Website without prior written approval:</p>
+      <Typography>The following organizations may link to our Website without prior written approval:</Typography>
 
-      <ul>
+      <Typography>
         <li>Government agencies;</li>
         <li>Search engines;</li>
         <li>News organizations;</li>
@@ -126,17 +128,19 @@ export function Terms() {
           System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and
           charity fundraising groups which may not hyperlink to our Web site.
         </li>
-      </ul>
+      </Typography>
 
-      <p>
+      <Typography>
         These organizations may link to our home page, to publications or to other Website information so long as the
         link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the
         linking party and its products and/or services; and (c) fits within the context of the linking party's site.
-      </p>
+      </Typography>
 
-      <p>We may consider and approve other link requests from the following types of organizations:</p>
+      <Typography>
+        We may consider and approve other link requests from the following types of organizations:
+      </Typography>
 
-      <ul>
+      <Typography sx={{ m: 3 }}>
         <li>commonly-known consumer and/or business information sources;</li>
         <li>dot.com community sites;</li>
         <li>associations or other groups representing charities;</li>
@@ -144,117 +148,117 @@ export function Terms() {
         <li>internet portals;</li>
         <li>accounting, law and consulting firms; and</li>
         <li>educational institutions and trade associations.</li>
-      </ul>
+      </Typography>
 
-      <p>
+      <Typography>
         We will approve link requests from these organizations if we decide that: (a) the link would not make us look
         unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative
         records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of Project
         Corporation ; and (d) the link is in the context of general resource information.
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not
         falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c)
         fits within the context of the linking party's site.
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website,
         you must inform us by sending an e-mail to Project Corporation . Please include your name, your organization
         name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to
         our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.
-      </p>
+      </Typography>
 
-      <p>Approved organizations may hyperlink to our Website as follows:</p>
+      <Typography>Approved organizations may hyperlink to our Website as follows:</Typography>
 
-      <ul>
+      <Typography sx={{ m: 3 }}>
         <li>By use of our corporate name; or</li>
         <li>By use of the uniform resource locator being linked to; or</li>
         <li>
           By use of any other description of our Website being linked to that makes sense within the context and format
           of content on the linking party's site.
         </li>
-      </ul>
+      </Typography>
 
-      <p>
+      <Typography>
         No use of Project Corporation 's logo or other artwork will be allowed for linking absent a trademark license
         agreement.
-      </p>
+      </Typography>
 
-      <h3>
-        <strong>iFrames</strong>
-      </h3>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        iFrames
+      </Typography>
 
-      <p>
+      <Typography>
         Without prior approval and written permission, you may not create frames around our Webpages that alter in any
         way the visual presentation or appearance of our Website.
-      </p>
+      </Typography>
 
-      <h3>
-        <strong>Content Liability</strong>
-      </h3>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        Content Liability
+      </Typography>
 
-      <p>
+      <Typography>
         We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend
         us against all claims that is rising on your Website. No link(s) should appear on any Website that may be
         interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the
         infringement or other violation of, any third party rights.
-      </p>
+      </Typography>
 
-      <h3>
-        <strong>Reservation of Rights</strong>
-      </h3>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        Reservation of Rights
+      </Typography>
 
-      <p>
+      <Typography>
         We reserve the right to request that you remove all links or any particular link to our Website. You approve to
         immediately remove all links to our Website upon request. We also reserve the right to amen these terms and
         conditions and it's linking policy at any time. By continuously linking to our Website, you agree to be bound to
         and follow these linking terms and conditions.
-      </p>
+      </Typography>
 
-      <h3>
-        <strong>Removal of links from our website</strong>
-      </h3>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        Removal of links from our website
+      </Typography>
 
-      <p>
+      <Typography>
         If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any
         moment. We will consider requests to remove links but we are not obligated to or so or to respond to you
         directly.
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         We do not ensure that the information on this website is correct, we do not warrant its completeness or
         accuracy; nor do we promise to ensure that the website remains available or that the material on the website is
         kept up to date.
-      </p>
+      </Typography>
 
-      <h3>
-        <strong>Disclaimer</strong>
-      </h3>
+      <Typography sx={{ fontWeight: 700 }} variant="h4">
+        Disclaimer
+      </Typography>
 
-      <p>
+      <Typography>
         To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions
         relating to our website and the use of this website. Nothing in this disclaimer will:
-      </p>
+      </Typography>
 
-      <ul>
+      <Typography sx={{ m: 3 }}>
         <li>limit or exclude our or your liability for death or personal injury;</li>
         <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
         <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
         <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
-      </ul>
+      </Typography>
 
-      <p>
+      <Typography>
         The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are
         subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including
         liabilities arising in contract, in tort and for breach of statutory duty.
-      </p>
+      </Typography>
 
-      <p>
+      <Typography>
         As long as the website and the information and services on the website are provided free of charge, we will not
         be liable for any loss or damage of any nature.
-      </p>
-    </div>
+      </Typography>
+    </Box>
   );
 }
